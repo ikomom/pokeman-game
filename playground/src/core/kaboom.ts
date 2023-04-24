6c02d0ece117092b6f2e57e3b0773fa984d114f7
yzm <315793076@qq.com>
2023-04-24 09:38:02 +0800

    refactor: remove kaboom code

--- playground/src/core/kaboom.ts
+++
@@ -1,8 +0,0 @@
-import type { KaboomOpt } from 'kaboom'
-import kaboom from 'kaboom'
-
-export function createInstance(options?: KaboomOpt) {
-  return kaboom({
-    ...options,
-  })
-}
--- playground/src/core/types.ts
+++
@@ -1,7 +0,0 @@
-import type { Vec2 } from 'kaboom'
-
-export interface WorldState {
-  enemyName: string
-  faintedMons: string[]
-  playerPos: Vec2
-}
